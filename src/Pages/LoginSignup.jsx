8bdc98d80ad19e340eeb8508a806f3ef21cd58a6
vanashreetajane <159805880+vanashreetajane@users.noreply.github.com>
2024-02-20 16:45:38 +0530

    update

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import './CSS/LoginSignup.css'
 const LoginSignup = () => {
   return (
-    <div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password"  placeholder='Password'/>
+        </div>
+        <p className="loginsignupp">Already have an account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>I agree to the Terms and Conditions</p>
+        </div>
+        <button>Continue</button>
+       
+      </div>
       
     </div>
   )
